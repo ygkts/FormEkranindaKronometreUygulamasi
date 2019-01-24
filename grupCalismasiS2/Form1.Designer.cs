b6b9eb14d1cb64ca5b9ae6678696026803daee54
yase <ygkts61@gmail.com>
2019-01-24 23:16:09 +0300

    ffff

---
+++ grupCalismasiS2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace grupCalismasiS2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnDur = new System.Windows.Forms.Button();
+            this.btnSifirla = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(191, 91);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(75, 23);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnDur
+            // 
+            this.btnDur.Location = new System.Drawing.Point(191, 163);
+            this.btnDur.Name = "btnDur";
+            this.btnDur.Size = new System.Drawing.Size(75, 23);
+            this.btnDur.TabIndex = 1;
+            this.btnDur.Text = "durdur";
+            this.btnDur.UseVisualStyleBackColor = true;
+            this.btnDur.Click += new System.EventHandler(this.btnDur_Click);
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.Location = new System.Drawing.Point(191, 241);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(75, 23);
+            this.btnSifirla.TabIndex = 2;
+            this.btnSifirla.Text = "sıfırla";
+            this.btnSifirla.UseVisualStyleBackColor = true;
+            this.btnSifirla.Click += new System.EventHandler(this.btnSifirla_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(45, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(115, 173);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSifirla);
+            this.Controls.Add(this.btnDur);
+            this.Controls.Add(this.btnBasla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnDur;
+        private System.Windows.Forms.Button btnSifirla;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ grupCalismasiS2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grupCalismasiS2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int saat = 0, dakika = 0, saniye = 0;
+        string sonHal;
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void btnDur_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void btnSifirla_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            saat = 0;
+            dakika = 0;
+            saniye = 0;
+           
+            sonHal = label1.Text;
+            listBox1.Items.Add(sonHal);
+            label1.Text = "0 : 0 : 0";
+            
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            label1.Text = ((Convert.ToString(saniye) + " : ") + (Convert.ToString(dakika) + " : ") + Convert.ToString(saat));
+            if ((saniye == 59))
+            {
+                saniye = 0;
+                dakika = dakika + 1;
+                if (dakika == 60)
+                {
+                    saniye = 0;
+                    dakika = 0;
+                    saat = saat + 1;
+                }
+            }
+            saniye = saniye + 1;
+
+            
+        }
+    
+    }
+}
